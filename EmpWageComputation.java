9c801c324235afe5e0dd54eb33146ccfbcf30fee
samiksha.karmakar96@gmail.com <samiksha.karmakar96@gmail.com>
2020-04-27 21:54:42 +0530

    calculate wage till 100 hours or 20 days reached use case 6

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -22,20 +22,31 @@ class Employee{
       }
    }
 
-	public int get_monthly_salary(int Hours, int day)
+	public int[] get_monthly_salary(int Hours, int day, int type)
    {
 		int status;
+		int hrs_worked=0;
 		Random rand = new Random();
 		status = rand.nextInt(2);
+		int[] salary_and_hours = new int[2];
 		if(status==0){
-			System.out.println("Day "+day+": Present");								/*if employee present on that day*/
-      	salary=WagePrHr*Hours;
-      	return salary;
+			System.out.println("Day "+day+": Present");
+			salary+=WagePrHr*Hours;
+			if(type==0){
+				hrs_worked+=8;}
+			if(type==1){
+				hrs_worked+=4;}
+			salary_and_hours[0] = salary;
+			salary_and_hours[1] = hrs_worked;
+			return salary_and_hours;
 		}
 		else{
 			System.out.println("Day "+day+": Absent");
-			salary=0;																			/*if employee absent on that day*/
-			return salary;
+			salary+=0;
+			hrs_worked+=0;
+			salary_and_hours[0] = salary;
+         salary_and_hours[1] = hrs_worked;
+         return salary_and_hours;
 		}
    }
 
@@ -53,19 +64,31 @@ class EmpWageComputation{
    public static void main(String []s){
 		int empType;
 		int DailyWage;
-		int MonthWage=0;
 		int WorkHrs;
-		int get_wage;
+		int TotalDays=1;
+		int loop_end=0;
+		int [] salary_and_hours = new int[2];
       System.out.println("Welcome to Employee Wage Computation Program");
 		Employee person = new Employee();
 		Random rand = new Random();
 		empType = rand.nextInt(2);
 		WorkHrs = person.get_emp_type(empType);
 		DailyWage = person.get_daily_salary(WorkHrs);
-      for (int i=1;i<=20;i++){
-      MonthWage += person.get_monthly_salary(WorkHrs,i);
-		//MonthWage = MonthWage+get_wage;
+		System.out.println("The daily wage is: "+DailyWage);
+      while (loop_end==0){
+      salary_and_hours = person.get_monthly_salary(WorkHrs,TotalDays,empType);
+		if(TotalDays==20 || salary_and_hours[1]==100){
+			if(TotalDays==20){
+				System.out.println("20 days over!");
+				break;
+				}
+			else{
+				System.out.println("100 working hours completed!");
+				break;
+				}
+			}
+			TotalDays+=1;
       }
-      System.out.println("The monthly wage (20 dasys) is: "+ MonthWage);
+      System.out.println("The monthly wage is: "+ salary_and_hours[0]);
    }
 }
