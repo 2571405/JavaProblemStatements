9d2377486aab46aa815d135e39be7cd0b182e3da
2571405 <63220453+2571405@users.noreply.github.com>
2020-04-28 20:49:56 +0530

    added comments UC4

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,45 +1,67 @@
+/*
+*Employee Wage Computation Program Use Case 4
+*solve using switch case
+*@author Moutusi Karmakar
+*/
 import java.util.Random;
+/*Employee utility class*/
 class Employee{
-	int salary;
-	int attendance;
-
-	public int get_salary(int attendance)
-	{
-		int WagePrHr=20;
-		int DayPrHr=2;
-		this.attendance = attendance;
-		switch(attendance)
-		{
-		case 0:
-			System.out.println("Full time employee");
-         DayPrHr = 8;
-		break;
-		case 1:
-			System.out.println("Part time employee");
-			DayPrHr = 4;
-		break;
-		case 2:
-			System.out.println("Employee absent");
-			DayPrHr = 0;
-		break;
-		default:
-			System.out.println(" ");
-		break;
-		}
-		salary=WagePrHr*DayPrHr;
-		return salary;
-	}
-
+int salary;
+int attendance;
+final int WagePrHr;
+/*
+*Constructor Employee
+*initialises the final variable WagePrHr
+*/
+Employee() {
+   WagePrHr=20;
 }
+      /*
+      *Method that returns daily wage of employee using switch case	
+      *@param attendance	
+      */
+      public int getDailyWage(int attendance){
+	 int DayPrHr=2;
+	 this.attendance = attendance;
+	 switch(attendance){
+	  case 0:
+	    System.out.println("Full time employee");
+            DayPrHr = 8;
+	  break;
+	  case 1:
+	     System.out.println("Part time employee");
+	     DayPrHr = 4;
+	  break;
+	  case 2:
+	     System.out.println("Employee absent");
+	     DayPrHr = 0;
+	  break;
+	  default:
+	     System.out.println(" ");
+	  break;
+	 }
+	 salary=WagePrHr*DayPrHr;
+	 return salary;
+      }
+}
+/*
+*Employee Wage Computation class
+*Main method class
+*/
 class EmpWageComputation{
-   public static void main(String []s){
-		int attend;
-		int DailyWage;
-      System.out.println("Welcome to Employee Wage Computation Program");
-		Employee person = new Employee();
-		Random rand = new Random();
-		attend = rand.nextInt(3);
-		DailyWage = person.get_salary(attend);
-		System.out.println("The Daily wage is: "+DailyWage);
-   }
+      /*     
+      *Main method     
+      *@param s     
+      */
+      public static void main(String []s){
+	 int attend;
+	 int DailyWage;
+         System.out.println("Welcome to Employee Wage Computation Program");
+	 Employee person = new Employee();
+	 Random rand = new Random();
+	 attend = rand.nextInt(3);
+	 /*Storing the wage returned from the getDailyWage method in DailyWage variable*/
+	 DailyWage = person.getDailyWage(attend);
+	 System.out.println("The Daily wage is: "+DailyWage);
+      }
 }
