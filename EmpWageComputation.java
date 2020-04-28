35ca5672d0882284e2e6ecdd4a7cdc3aa8c8517e
2571405 <63220453+2571405@users.noreply.github.com>
2020-04-28 19:30:49 +0530

    comments added

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,27 +1,41 @@
 import java.util.Random;
+/*
+*Employee utility class
+*Author Moutusi Karmakar
+*/
 class Employee{
-	int salary;
-	int attendance;
-
-	public void check_attendance(int attendance)
-	{
-		this.attendance = attendance;
-		if(this.attendance==0){
-			System.out.println("Employee Present");
-		}
-		else{
-			System.out.println("Employee Absent");
-		}
-	}
-
+    int salary;
+    int attendance;
+    /*
+    *Method to check the attendance of an employee
+    *@arg attendance
+    */
+    public void checkAttendance(int attendance){
+	this.attendance = attendance;
+	if(this.attendance==0)
+		System.out.println("Employee Present");
+	else
+		System.out.println("Employee Absent");
+     }
 }
+/*
+*Employee Wage Computation class
+*Main method class
+*@author Moutusi Karmakar
+*/
 class EmpWageComputation{
+   /*
+   *Main method
+   *@param s
+   */
    public static void main(String []s){
-		int attend;
-      System.out.println("Welcome to Employee Wage Computation Program");
-		Employee person = new Employee();
-		Random rand = new Random();
-		attend = rand.nextInt(2);
-		person.check_attendance(attend);
+     int attend;
+     System.out.println("Welcome to Employee Wage Computation Program");
+     /*Instantiating the Employee class*/
+     Employee person = new Employee();
+     Random rand = new Random();
+     attend = rand.nextInt(2);
+     /*Calling method to check attendance and printing present or absent*/
+     person.checkAttendance(attend);
    }
 }
