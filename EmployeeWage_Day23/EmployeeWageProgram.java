43ffdc1c08ace4090ed85c5824619a8d2f48dc3f
samiksha.karmakar96@gmail.com <samiksha.karmakar96@gmail.com>
2020-05-01 15:56:18 +0530

    added

---
+++ EmployeeWage_Day23/EmployeeWageProgram.java
@@ -0,0 +1,95 @@
+/*
+*Employee Wage Computation Program
+*using instance method and instance variables
+*@author Moutusi Karmakar
+*/
+import java.util.Random;
+//Employee Wage class
+public class EmployeeWageProgram{
+//Constants
+private static final int IS_PART_TIME = 0;
+private static final int IS_FULL_TIME = 1;
+private int numOfCompany = 0;
+private CompanyEmpWage[] companyEmpWageArray;
+
+//Constructor
+public EmployeeWageProgram(){
+	companyEmpWageArray = new CompanyEmpWage[3];
+}
+
+private void addCompanyEmpWage(String compName, int numOfDays, int empRate, int maxHrs){
+	companyEmpWageArray[numOfCompany] = new CompanyEmpWage(compName, numOfDays, empRate, maxHrs);
+	numOfCompany++;
+}
+	/*
+	*Method for calculating monthly wage of employee based on type (part time/ full time)
+	*calculates salary according to the attendance of the employee
+	*/
+     private void calculateEmpWage(){
+     int totalWorkingDays = 1;
+	  int totalWorkingHours = 0;
+     //variable empType tells the type of employee i.e., part time/full time
+	  int empType;
+	  //variable empHrs fetches the no of work hours per day according to type of employee
+	  int empHrs = 0;
+	  //varable empAttendance tells if emp is present '0' or absent '1' on that day of the month
+	  int empAttendance;
+     int monthlyWage = 0;
+	  //variable daysPresent keeps count of the no of days present for a month
+	  int daysPresent = 0;
+	  //variable hoursWorked keeps count of the no of hours worked in a month
+	  int hoursWorked = 0;
+	  Random rand = new Random();
+	  empType = rand.nextInt(2);
+	  switch(empType){
+	  case IS_PART_TIME:
+		System.out.println("PART TIME EMPLOYEE");
+		empHrs = 4;
+	  break;
+	  case IS_FULL_TIME:
+		System.out.println("FULL TIME EMPLOYEE");
+		empHrs = 8;
+	  break;
+	  default:
+	        System.out.println(" ");
+	  break;
+	  }
+	  while(true){
+          empAttendance = rand.nextInt(2);
+          if(empAttendance == 0){
+              daysPresent += 1;
+              System.out.println("Day "+totalWorkingDays+": Present");
+              monthlyWage += empRate * empHrs;
+              hoursWorked += empHrs;
+          }
+          else{
+              System.out.println("Day "+totalWorkingDays+": Absent");
+              monthlyWage += 0;
+              hoursWorked += 0;
+          }
+          if(totalWorkingDays == numOfDays || !(totalWorkingHours < maxHrs)){
+              if(totalWorkingDays == numOfDays){
+                   System.out.println(numOfDays+" days are over!");
+              break;
+              }
+              else{
+                   System.out.println(maxHrs+" hours reached!");
+              break;
+              }
+          }
+	  totalWorkingDays++;
+      	  totalWorkingHours += empHrs;
+         }
+      System.out.println("Company: "+compName+"\nNo of days worked out of "+numOfDays+" days: "+daysPresent+"\nNo of hours worked out of "+maxHrs+" hours: "+hoursWorked+"\nSalary for the month: "+monthlyWage);
+      }
+     /*
+     *Main method
+     *@param s
+     */
+      public static void main(String []s){
+			EmployeeWageProgram emp = new EmployeeWageProgram();
+	   	emp.addCompanyEmpWage("BridgeLabz",20,20,100);
+	   	emp.addCompanyEmpWage("DMart",25,15,150);
+	   	emp.addCompanyEmpWage("Big Basket",20,25,100);
+      }
+}
