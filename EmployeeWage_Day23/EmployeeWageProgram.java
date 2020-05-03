a1e52b819dfb850b921fbf196ce20cd7ff628a4c
samiksha.karmakar96@gmail.com <samiksha.karmakar96@gmail.com>
2020-05-04 01:06:32 +0530

    added video problem

--- EmployeeWage_Day23/EmployeeWageProgram.java
+++ EmployeeWage_Day23/EmployeeWageProgram.java
@@ -5,71 +5,81 @@
 */
 import java.util.Random;
 import java.util.*;
-//Employee Wage class
-public class EmployeeWageProgram{
+//Employee class
+class Employee{
 //Constants
 private static final int IS_PART_TIME = 0;
 private static final int IS_FULL_TIME = 1;
 private int numOfCompany = 0;
 private List<CompanyEmpWage> companyEmpWageArray;
 
-//Constructor
-public EmployeeWageProgram(){
+/*
+*Constructor Employee
+*/
+public Employee(){
 	companyEmpWageArray = new ArrayList<CompanyEmpWage>();
 }
 
-public void addCompanyEmpWage(String compName, int numOfDays, int empRate, int maxHrs){
-	CompanyEmpWage obj = new CompanyEmpWage(compName, numOfDays, empRate, maxHrs);
-	companyEmpWageArray.add(obj);
-}
-
-public int totalCompanies(){
-	return companyEmpWageArray.size();
-}
+	public void addCompanyEmpWage(String compName, int numOfDays, int empRate, int maxHrs){
+		CompanyEmpWage obj = new CompanyEmpWage(compName, numOfDays, empRate, maxHrs);
+		companyEmpWageArray.add(obj);
+	}
+	/*
+	*method that gives the total number of companies for which wages are calculated
+	*/
+	public int totalCompanies(){
+		return companyEmpWageArray.size();
+	}
 
 	/*
-	*Method for calculating monthly wage of employee based on type (part time/ full time)
-	*calculates salary according to the attendance of the employee
+	*method getType returns the employee hours per day according to type of employee
+	*@param type
 	*/
-     private void calculateEmpWage(){
-     Object totalWorkingDays = 1;
-	  Object totalWorkingHours = 0;
-     //variable empType tells the type of employee i.e., part time/full time
-	  int empType;
-	  //variable empHrs fetches the no of work hours per day according to type of employee
-	  Object empHrs = 0;
-	  //varable empAttendance tells if emp is present '0' or absent '1' on that day of the month
+	public int getType(int type){
+		int empHrs = 0;
+		if(type == IS_PART_TIME){
+			System.out.println("PART TIME EMPLOYEE");
+      	empHrs = 4;
+  		}
+   	if(type == IS_FULL_TIME){
+      	System.out.println("FULL TIME EMPLOYEE");
+      	empHrs = 8;
+   	}
+		return empHrs;
+	}
+
+
+
+	  /*Method for calculating monthly wage of employee based on type (part time/ full time)
+	  *calculates salary according to the attendance of the employee
+	  */
+     public void calculateEmpWage(int empHrs){
+     int totalWorkingDays = 1;
+	  int totalWorkingHours = 0;
+	  /*
+	  *varable empAttendance tells if emp is present '0' or absent '1' on that day of the month
+	  */
 	  int empAttendance;
+	  /*
+	  *variable monthlyWage stores the monthly wage of the employee
+	  */
      int monthlyWage = 0;
-	  //variable daysPresent keeps count of the no of days present for a month
+	  /*
+	  *variable daysPresent keeps count of the no of days present for a month
+		*/
 	  int daysPresent = 0;
-	  //variable hoursWorked keeps count of the no of hours worked in a month
+	  /*
+	  *variable hoursWorked keeps count of the no of hours worked in a month
+	  */
 	  int hoursWorked = 0;
-	  Random rand = new Random();
-	  empType = rand.nextInt(2);
-	  switch(empType){
-	  case IS_PART_TIME:
-		System.out.println("PART TIME EMPLOYEE");
-		empHrs = 4;
-	  break;
-	  case IS_FULL_TIME:
-		System.out.println("FULL TIME EMPLOYEE");
-		empHrs = 8;
-	  break;
-	  default:
-	        System.out.println(" ");
-	  break;
-	  }
-		Object CN = companyEmpWageArray.get(0);
-		Object ND = companyEmpWageArray.get(1);
-		Object ER = companyEmpWageArray.get(2);
-		Object MH = companyEmpWageArray.get(3);
+
 	  while(true){
+		 	 Random rand = new Random();
           empAttendance = rand.nextInt(2);
           if(empAttendance == 0){
               daysPresent += 1;
               System.out.println("Day "+totalWorkingDays+": Present");
-              monthlyWage += ER * empHrs;
+              monthlyWage += CompanyEmpWage.getempRate() * empHrs;
               hoursWorked += empHrs;
           }
           else{
@@ -77,33 +87,53 @@ public int totalCompanies(){
               monthlyWage += 0;
               hoursWorked += 0;
           }
-          if(totalWorkingDays == ND  || !(totalWorkingHours < MH)){
-              if(totalWorkingDays == ND){
-                   System.out.println(ND+" days are over!");
-              break;
+          if(totalWorkingDays == CompanyEmpWage.getnumOfDays()  || !(totalWorkingHours < CompanyEmpWage.getmaxHrs())){
+              if(totalWorkingDays == CompanyEmpWage.getnumOfDays()){
+                   System.out.println(CompanyEmpWage.getnumOfDays()+" days are over!");
+                   break;
               }
               else{
-                   System.out.println(MH+" hours reached!");
-              break;
+                   System.out.println(CompanyEmpWage.getmaxHrs()+" hours reached!");
+                   break;
               }
           }
-	  totalWorkingDays++;
-      	  totalWorkingHours += empHrs;
-         }
-      System.out.println("Company: "+CN+"\nNo of days worked out of "+ND+" days: "+daysPresent+"\nNo of hours worked out of "+MH+" hours: "+hoursWorked+"\nSalary for the month: "+monthlyWage);
+	  	totalWorkingDays++;
+      totalWorkingHours += empHrs;
+       }
+      System.out.println("Company: "+CompanyEmpWage.getcompName()+"\nNo of days worked out of "+CompanyEmpWage.getnumOfDays()+" days: "+daysPresent+"\nNo of hours worked out of "+CompanyEmpWage.getmaxHrs()+" hours: "+hoursWorked+"\nSalary for the month: "+monthlyWage);
       }
+}
+/*
+*Employee wage program class
+*/
+public class EmployeeWageProgram{
      /*
      *Main method
      *@param s
      */
       public static void main(String []s){
-			EmployeeWageProgram emp = new EmployeeWageProgram();
+			/*
+			*dayPrHr stores the working hours per day according to type
+			*/
+			int dayPrHr;
+			/*
+			*empType stores the type of employee (part time or full time)
+			*/
+			int empType;
+			Random rand = new Random();
+			Employee emp = new Employee();
 	   	emp.addCompanyEmpWage("BridgeLabz",20,20,100);
-			emp.calculateEmpWage();
+			empType = rand.nextInt(2);
+			dayPrHr = emp.getType(empType);
+			emp.calculateEmpWage(dayPrHr);
 	   	emp.addCompanyEmpWage("DMart",25,15,150);
-			emp.calculateEmpWage();
+			empType = rand.nextInt(2);
+         dayPrHr = emp.getType(empType);
+			emp.calculateEmpWage(dayPrHr);
 	   	emp.addCompanyEmpWage("Big Basket",20,25,100);
-			emp.calculateEmpWage();
+			empType = rand.nextInt(2);
+         dayPrHr = emp.getType(empType);
+			emp.calculateEmpWage(dayPrHr);
 			System.out.println("No of companies added: "+emp.totalCompanies());
       }
 }
